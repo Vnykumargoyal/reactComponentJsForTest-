b48b23d200501aac99dcf1cc65e9a777d933eb13
vinay kumar goyal <vinaykumar.goyal@decimal.co.in>
2020-12-22 23:09:53 +0530

    some change in style

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange = (event) => {
-        console.log(event.target.value );
-    }
+  state = { term: '' };
 
-    render() {
-        return (
-            <div className="ui sagment">
-                <from className="ui form">
-                    <div className="feild">
-                        <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange}/>
-                    </div>
-                </from>
-            </div>
-        )
-    }
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
